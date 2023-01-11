e598003041cf7a3a5eb77aa9e2c504abbd66d098
Milos Nastic <Milos98le@gmail.com>
2023-01-11 15:57:07 +0100

    fourth commit

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -3,8 +3,49 @@ import 'package:flutter/material.dart';
 class Landing extends StatelessWidget {
   const Landing({super.key});
 
+  final double logoHeight = 200.0;
+  final double logoWidth = 50.0;
+
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.red[900],
+                ),
+              ),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            left: (MediaQuery.of(context).size.width / 2),
+            top: (MediaQuery.of(context).size.height / 2),
+            child: Container(
+              height: logoHeight,
+              width: logoWidth,
+              decoration: const BoxDecoration(
+                  color: Colors.red,
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/logo.png"),
+                      fit: BoxFit.cover)),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
